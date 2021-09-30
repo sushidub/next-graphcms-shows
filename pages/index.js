f4bb287f5ebc33c42f95adfe3515ac4fb11e1c35
Jeremy Graston <jeremygraston@gmail.com>
2021-09-30 13:43:17 -0600

    wrapup preso (main) branch and commit for Dreamstage review

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,14 @@ import Layout from '@c/Layout'
 import { Grid, Card } from '@c/Grid'
 import { Title, SubTitle } from '@c/Title'
 
+
 export default function Home() {
   return (
     <Layout title="next-graphcms-shows / Home" maxWidth="800px">
       <Title>next-graphcms-shows</Title>
-
       <SubTitle>
         A Next.js application for displaying fictional live events retrieved from GraphCMS
       </SubTitle>
-
       <Grid>
         <Card href="/readme" header="README">
           <p>Check the README for details on how to work on this project.</p>
--- pages/shows.js
+++ pages/shows.js
@@ -1,19 +1,43 @@
+import { useState } from 'react'
 import Layout from '@c/Layout'
-import { Grid, Card } from '@c/Grid'
+import { Grid, List, Card } from '@c/Grid'
 import { Title } from '@c/Title'
 import { getAllShows } from '@l/graphcms'
 
 export default function Shows({ shows }) {
+  const [ showsDisplayState, setShowsDisplayState ] = useState(true); // true = Grid, false = List
+
+  function ToggleBtn() {
+    return (
+      <div>
+        <input className="toggle-btn" type="checkbox" value={showsDisplayState} onChange={setShowsDisplayState(!showsDisplayState)} />
+      </div>
+    );
+  }
+
   return (
     <Layout title="next-graphcms-shows / Shows">
       <Title>Shows</Title>
+
+      <ToggleBtn showState={showsDisplayState} />
+      {!showsDisplayState &&
+        <List>
+          {shows.map(show => (
+            <li href={`/show/${show.slug}`} header={show.title} key={show.id}>
+              <p>{show.artists.map(({ fullName }) => fullName).join(', ')}</p>
+            </li>
+          ))}
+        </List>
+      }
+
+      {showsDisplayState &&
       <Grid>
         {shows.map(show => (
           <Card href={`/show/${show.slug}`} header={show.title} key={show.id}>
             <p>{show.artists.map(({ fullName }) => fullName).join(', ')}</p>
           </Card>
         ))}
-      </Grid>
+      </Grid> }
     </Layout>
   )
 }
